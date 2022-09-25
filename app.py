7b86ec20b852d8b54e740189ee7fcdc809faf3e5
Alfy-G-Varghese <48632437+Alfy-G-Varghese@users.noreply.github.com>
2022-09-25 18:18:27 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+      <meta charset="utf-8">
+      <title> Hw3 </title>
+        </head>
+        <body>
+          <h1> Alfy Varghese\'s HW3</h1>
+      <p> sql_version_function() </p>
+        </body>
+        </html>"""
+
+if __name__ == "__main__":
+    app.run()
