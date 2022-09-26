70f32aee2d9b6d237843e623d32d1e8defb5d885
Alfy-G-Varghese <48632437+Alfy-G-Varghese@users.noreply.github.com>
2022-09-26 08:57:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hw3')
+@app.route('/')
 def index():
-    return """<!DOCTYPE html>
-        <html lang="en">
-        <head>
-      <meta charset="utf-8">
-      <title> Hw3 </title>
-        </head>
-        <body>
-          <h1> Alfy Varghese\'s HW3</h1>
-      <p> sql_version_function() </p>
-        </body>
-        </html>"""
+    return '''Alfy Varghese\'s HW3
 
-if __name__ == "__main__":
-    app.run()
+<p> sql_version_function() </p>'''
